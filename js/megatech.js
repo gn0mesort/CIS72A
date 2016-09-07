b4894565be9507b054de86cf595263a33e1e37d9
gn0mesort <gnomesort@megate.ch>
2016-09-07 12:35:39 -0700

    Signed-off-by: gn0mesort <gnomesort@megate.ch>

--- js/megatech.js
+++ js/megatech.js
@@ -1,3 +1,18 @@
-megatech = {
-	Clear : function(element){ element.innerHTML = ""; }
-}
+var megatech = (function(){
+	var linkCount = 0;
+	var rainbow = function(element) {
+		for(var i = 0; i < 256; ++i){
+			element.style.color = "#" + i.toString(16);
+		}
+	};
+	return {
+		clear : function(element){ element.innerHTML = ""; },
+		insertLink : function(element, link){ 
+			element.innerHTML += "<p id='link" + linkCount++ + "'><a href='" + link + "'>" + link + "</a></p>"; 
+		},
+		startRainbow : function(element) {
+			element.attributes["rainbow"] = true;
+			rainbow(element);
+		}
+	};
+})();
