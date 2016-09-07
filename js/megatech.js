4d1d0269dcded78410e6958c25bd7bb55f263f5c
gn0mesort <gnomesort@megate.ch>
2016-09-07 15:16:19 -0700

    Signed-off-by: gn0mesort <gnomesort@megate.ch>

--- js/megatech.js
+++ js/megatech.js
@@ -1,18 +1,27 @@
 var megatech = (function(){
 	var linkCount = 0;
+	var curColor = 0;
+	var colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+	var rainbowId = 0;
 	var rainbow = function(element) {
-		for(var i = 0; i < 256; ++i){
-			element.style.color = "#" + i.toString(16);
-		}
+			element.style.color = colors[curColor++];
+			if(curColor > colors.length) { curColor = 0; }
+			//element.style.color = "#" + curColor.toString(16);
+			//++curColor;
+			//if(curColor > 255) { curColor = 0; }
 	};
+	
 	return {
 		clear : function(element){ element.innerHTML = ""; },
 		insertLink : function(element, link){ 
 			element.innerHTML += "<p id='link" + linkCount++ + "'><a href='" + link + "'>" + link + "</a></p>"; 
 		},
-		startRainbow : function(element) {
-			element.attributes["rainbow"] = true;
-			rainbow(element);
+		startRainbow : function(element){
+			if(rainbowId === 0){ rainbowId = setInterval(rainbow, 75, element); }
+			else { 
+				clearInterval(rainbowId);
+				rainbowId = 0;
+			}
 		}
 	};
 })();
