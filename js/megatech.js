586ec21c346daccf3c39818420318479351b80c2
gn0mesort <gnomesort@megate.ch>
2016-09-14 12:19:21 -0700

    Signed-off-by: rcc <rcc>

--- js/megatech.js
+++ js/megatech.js
@@ -7,13 +7,37 @@ var megatech = (function(){
 			element.style.color = colors[curColor++];
 			if(curColor > colors.length) { curColor = 0; }
 	};
+	var processRps = function(data){
+			if(data === 0){ return "../img/rock.jpg"; }
+			else if (data === 1){ return "../img/paper.jpg"; }
+			else if (data === 2){ return "../img/scissors.jpg"; }
+			else { console.log("Invalid Data"); }
+	};
 	
 	return {
 		random : function(obj = {min : 0, max : 100}){
 			return Math.floor((Math.random() * obj.max) + obj.min);
-		}
-		rps : function(element){
-		}
+		},
+		rps : function(data){
+			var player = document.getElementById("player");
+			var cpu = document.getElementById("cpu");
+			player.src = processRps(data);
+			player.choice = data;
+			cpu.choice = megatech.random({min : 0, max : 3});
+			cpu.src = processRps(cpu.choice);
+			if(player.choice !== cpu.choice){
+				if(player.choice === 0 && cpu.choice === 1) { return 0; }
+				else if(player.choice === 0 && cpu.choice == 2){ return 1; }
+				else if(player.choice === 1 && cpu.choice == 0){ return 1; }
+				else if(player.choice === 1 && cpu.choice == 2){ return 0; }
+				else if(player.choice === 2 && cpu.choice == 0){ return 0; }
+				else if(player.choice === 2 && cpu.choice == 1){ return 1; }
+				else { return -1; }
+			}
+			else{
+				return -1;
+			}
+		},
 		loadPug : function() {
 			element = document.getElementById("pug");
 			element.innerHTML = "<p style='text-align: center;'><img src='../img/pug.jpg' height='192' width='108'/></p>" + element.innerHTML;
